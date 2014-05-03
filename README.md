3dd0388550541005d7e0de6738fa5835a0d60406
Dan Patrick <dan@mdpatrick.com>
2014-05-03 15:31:59 -0700

    Working patreon user fetch task

---
+++ README.md
@@ -0,0 +1,37 @@
+
+
+# I needed a way to download the Patreon user data.
+
+So I created a web crawler task to do so. The task uses my environment variables (PATREON_EMAIL, and PATREON_PASSWORD) to authenticate, fetch the CSV, filter it, and return it as a proper Ruby hash.
+
+This is a substitute for using a proper API, which I'm under the impression Patreon might themselves eventually release.
+
+## Instructions
+
+First..
+Add Mechanize to your Gemfile, and make sure to install it with `bundle install`.
+
+Next, set your environment variables.
+
+```bash
+export PATREON_EMAIL="my@email.com"
+export PATREON_PASSWORD="mypassword"
+```
+
+Finally, create a task (or something) to use the scraper. Here's an example:
+
+```ruby
+require "#{Rails.root}/lib/scraper.rb"
+
+namespace :mywebsite do
+  task :process_patreon_users do
+    agent = Mechanize.new
+    Scraper::Patreon::authenticate(agent)
+
+    patreon = Scraper::Patreon::fetch_user_hash_from_csv(agent)
+    puts patreon[:name].inspect # A beautiful array of your patreon pledger's names.
+    # Also try: patreon[:email], patreon[:pledge], patreon[:lifetime], patreon[:status], patreon[:twitter]
+    #      .... and patreon[:shipping], patreon[:start], patreon[:MaxAmount]
+  end
+end
+```
---
+++ lib/scraper.rb
@@ -0,0 +1,47 @@
+require 'CSV'
+module Scraper
+  module Patreon
+    def self.authenticate(agent)
+      agent.get('http://www.patreon.com/login_i')
+      form = agent.page.forms.first
+      form.email = ENV['PATREON_EMAIL']
+      form.password = ENV['PATREON_PASSWORD']
+
+      form.submit form.buttons.first #return
+    end
+
+    def self.fetch_user_hash_from_csv(agent)
+      agent.get('https://www.patreon.com/downloadCsv?hid=')
+      csv_table = ::CSV.parse(agent.page.body)
+
+      hash = {
+          :names => [],
+          :original => CSV.parse(agent.page.body, {:headers => true}),
+          :skipsies => [], # Rows with non-data in them
+      }
+      i = 0
+
+      # Determine rows that need to be skipped because they have junk/non-user data.
+      hash[:original]["Name"].to_a.select do |name|
+        if (name.match /Reward/)
+          hash[:skipsies] << i
+        end
+        i += 1
+      end
+
+      # Set each column of the CSV to a symbol, but filter out "skipsies"
+      hash[:original].headers.each do |header_name|
+        header_name.downcase!
+        hash[header_name.to_sym] = []
+        # Add to hash, but dont include skipsies
+        hash[:original][header_name.to_s].to_a.each_with_index do |value,eq|
+          unless hash[:skipsies].include? eq
+            hash[header_name.to_sym] << value
+          end
+        end
+      end
+
+      hash #return
+    end
+  end
+end
